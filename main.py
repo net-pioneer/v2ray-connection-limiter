747f77f2cce7091a53ed35ea1188ade65a395ea7
kulkapis <117336554+kulkapis@users.noreply.github.com>
2022-11-04 23:02:43 -0700

    Update main.py
    The entire script is modified but the semantic is intact.

--- main.py
+++ main.py
@@ -1,71 +1,102 @@
-from glob import glob
-import os;
-import sqlite3;
-import time;
-import requests;
-import subprocess;
-import threading;
-import schedule;
-_db_address = '/etc/x-ui/x-ui.db'
-_max_allowed_connections = 1
-_user_last_id = 0;
-def getUsers():
-    global _user_last_id
-    conn = sqlite3.connect(_db_address)
-    cursor = conn.execute("select id,remark,port from inbounds where id > "+str(_user_last_id));
-    users_list = [];
+import os
+import sqlite3
+import time
+import requests
+import threading
+import schedule
+import logging
+import argparse
+
+logger = logging.getLogger('x-ui')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def getUsers(db_address, user_last_id):
+    conn = sqlite3.connect(db_address)
+    cursor = conn.execute(f"select id,remark,port from inbounds where id > {user_last_id}")
+    users_list = list()
     for c in cursor:
         users_list.append({'name':c[1],'port':c[2]})
-        _user_last_id = c[0];
-    conn.close();
+        user_last_id = c[0]
+    conn.close()
     return users_list
 
-def disableAccount(user_port):
-    conn = sqlite3.connect(_db_address) 
-    conn.execute("update inbounds set enable = 0 where port="+str(user_port));
+def disableAccount(user_port, db_address):
+    conn = sqlite3.connect(db_address) 
+    conn.execute(f"update inbounds set enable = 0 where port={user_port}")
     conn.commit()
-    conn.close();
+    conn.close()
     time.sleep(2)
     os.popen("x-ui restart")
     time.sleep(3)
     
-def checkNewUsers():
-    conn = sqlite3.connect(_db_address)
-    cursor = conn.execute("select count(*) from inbounds WHERE id > "+str(_user_last_id));
-    new_counts = cursor.fetchone()[0];
-    conn.close();
+def checkNewUsers(db_address, user_last_id):
+    conn = sqlite3.connect(db_address)
+    cursor = conn.execute(f"select count(*) from inbounds WHERE id > {user_last_id}")
+    new_counts = cursor.fetchone()[0]
+    conn.close()
     if new_counts > 0:
-        init()
+        init(args)
 
-def init():
-    users_list = getUsers();
-    for user in users_list:
-        thread = AccessChecker(user)
+def init(args):
+    for user in getUsers(args.db_address, args.user_last_id):
+        thread = AccessChecker(user, args)
         thread.start()
-        print("starting checker for : " + user['name'])
+        logger.info(f"starting checker for : {user['name']}")
+
+
 class AccessChecker(threading.Thread):
-    def __init__(self, user):
+    def __init__(self, user, args):
         threading.Thread.__init__(self)
-        self.user = user;
+        self.user = user
+        self.max_allowed_connections = args.max_allowed_connections
+        self.telegram_bot_token = args.telegram_bot_token
+        self.telegram_channel_id = args.telegram_channel_id
+        self.max_connections = dict()
+
     def run(self):
-        #global _max_allowed_connections; <<if you get variable error uncomment this.
-        user_remark = self.user['name'];
-        user_port = self.user['port'];
+        user_remark = self.user['name']
+        user_port = self.user['port']
         while True:
-            netstate_data =  os.popen("netstat -np 2>/dev/null | grep :"+str(user_port)+" | awk '{if($3!=0) print $5;}' | cut -d: -f1 | sort | uniq -c | sort -nr | head").read();
+            netstate_msg = f"netstat -np 2>/dev/null | grep :{user_port} |"
+            netstate_msg += " awk '{if($3!=0) print $5;}' | cut -d: -f1 | sort | uniq -c | sort -nr | head"
+            netstate_data =  os.popen(netstate_msg).read()
             netstate_data = str(netstate_data)
-            connection_count =  len(netstate_data.split("\n")) - 1;
-            #print("c "+str(user_port) + "-"+ str(connection_count))
-            if connection_count > _max_allowed_connections:
-                requests.get('https://api.telegram.org/[bot_token]/sendMessage?chat_id=[chat_id]&text='+user_remark+'%20locked')
-                disableAccount(user_port=user_port)
-                print("inbound with port " + str(user_port) + " blocked")
+            connection_count =  len(netstate_data.split("\n")) - 1
+            if connection_count > self.max_allowed_connections:
+                if not user_remark in self.max_connections.keys() or connection_count > self.max_connections[user_remark]:
+                    self.max_connections[user_remark] = connection_count
+                    logger.info(f"inbound {user_remark} with port {user_port} and {connection_count} connections was blocked!")
+                    if self.telegram_channel_id is not None and self.telegram_bot_token is not None:
+                        msg = 'https://api.telegram.org/bot'
+                        msg += self.telegram_bot_token
+                        msg += '/sendMessage?chat_id='
+                        msg += self.telegram_channel_id
+                        msg += '&text='
+                        msg += f'{user_remark} has {connection_count} connections and will be locked'.replace(' ', '%20')
+                        requests.get(msg)
             else:
                 time.sleep(2)
 
-init();
-schedule.every(10).minutes.do(checkNewUsers)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@', description="")
+
+    parser.add_argument('--db-address', default='/etc/x-ui/x-ui.db', help='')
+    parser.add_argument('--max-allowed-connections', type=int, default=1, help='')
+    parser.add_argument('--telegram-bot-token', type=str, default=None, help='')
+    parser.add_argument('--telegram-channel-id', type=str, default=None, help='')
+    parser.add_argument('--user-last-id', type=int, default=0, help='')
+   
+    args = parser.parse_args()
+
+    init(args)
+    schedule.every(10).minutes.do(checkNewUsers, args.db_address, args.user_last_id)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
